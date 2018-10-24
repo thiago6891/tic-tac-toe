4038ea629c88749b04a96c9bf4b302e965754fdb
Thiago Cordeiro <thiago6891@gmail.com>
2018-10-24 09:52:08 -0300

    versus game implemented. close #2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
         super(props);
 
         this.state = {
-            "dropdownText": DROPDOWN_TEXT_VERSUS
+            board: Array(9).fill(null),
+            currentMark: 'X',
+            winningLine: null,
+            dropdownText: DROPDOWN_TEXT_VERSUS
         };
     }
 
@@ -16,9 +19,61 @@ class App extends Component {
         this.setState({"dropdownText": text});
     }
 
+    handleBoxClick(n) {
+        let winner = this.getWinner(this.state.board);
+
+        if (winner === null) {
+            const board = this.state.board.slice();
+            board[n] = this.state.currentMark;
+
+            winner = this.getWinner(board);
+
+            if (winner) {
+                let line = winner.line;
+                this.setState({board: board, currentMark: null, winningLine: line});
+            } else {
+                let nextMark = this.state.currentMark === 'X' ? 'O' : 'X';
+                this.setState({board: board, currentMark: nextMark});
+            }
+        }
+    }
+
+    getWinner(board) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                return { mark: board[a], line: [a, b, c] };
+            }
+        }
+
+        return null;
+    }
+
+    restartGame() {
+        this.setState({board: Array(9).fill(null), currentMark: 'X', winningLine: null});
+    }
+
     render() {
+        let info = this.state.currentMark + " Turn";
+        let winner = this.getWinner(this.state.board);
+        if (winner) {
+            info = winner.mark + " Wins";
+        }
+
         return (
             <div className="container" style={{"max-width": "700px"}}>
+                
                 <div className="row mt-3 mb-3 ml-1">
                     <div className="dropdown">
                         <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
@@ -32,18 +87,26 @@ class App extends Component {
                         </div>
                     </div>
                 </div>
+                
                 <div className="row justify-content-center">
-                    <button className="btn btn-primary btn-sm font-weight-bold mr-1" disbaled="false">X</button>
-                    <button className="btn btn-primary btn-sm font-weight-bold ml-1" disabled="true">O</button>
+                    <button className="btn btn-primary btn-sm font-weight-bold mr-1" 
+                        disabled={this.state.currentMark !== 'X'}>X</button>
+                    <button className="btn btn-primary btn-sm font-weight-bold ml-1" 
+                        disabled={this.state.currentMark !== 'O'}>O</button>
                 </div>
+                
                 <div className="row justify-content-center mt-2">
-                    <p><small>Info Here</small></p>
+                    <p><small>{info}</small></p>
                 </div>
+                
                 <div className="row justify-content-center">
-                    <GameBoard />
+                    <GameBoard handleClick={(n) => this.handleBoxClick(n)} 
+                        board={this.state.board} 
+                        winningLine={this.state.winningLine} />
                 </div>
+                
                 <div className="row justify-content-center mt-3">
-                    <button className="btn btn-secondary">Restart Game</button>
+                    <button className="btn btn-secondary" onClick={() => this.restartGame()}>Restart Game</button>
                 </div>
             </div>
         )
